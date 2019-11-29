78db6e910ad6ab621a88f75a340011e218ae27f6
dedtig <51795381+dedtig@users.noreply.github.com>
2019-11-29 13:10:56 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -60,8 +60,9 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    pred_class,pred_idx,outputs = learn.predict(img)
+    prediction = f"Soort : {pred_class}\nConfidence : {outputs[pred_idx]:.2%}"
+    return JSONResponse(prediction)
 
 
 if __name__ == '__main__':
